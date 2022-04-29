84bf72043e9ba7b31e832ebe48547f6b2dcfdd38
nadjieb08 <97201898+nadjieb08@users.noreply.github.com>
2022-04-30 05:33:02 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
+╠➥ *6282234343854*
 ║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6282234343854
+╠═〘 Jibb 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
